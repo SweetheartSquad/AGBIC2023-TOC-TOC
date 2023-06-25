7797a773cab2dea930207ea44bbf3189baf47b5a
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-06-24 20:20:58 -0400

    dither test

--- src/assets/postprocess.frag.glsl
+++ src/assets/postprocess.frag.glsl
@@ -14,9 +14,9 @@ uniform vec2 camPos;
 uniform sampler2D ditherGridMap;
 uniform vec3 bg;
 uniform vec3 fg;
-const vec2 ditherSize = vec2(8.0);
+const vec2 ditherSize = vec2(64.0);
 const float scale = 1.0;
-const float posterize = 8.0;
+const float posterize = 1.0;
 const float brightness = 1.0;
 const float contrast = 1.0;
 
@@ -31,21 +31,23 @@ void main(void) {
 	vec2 uvDither = fract((coord + vec2(0.5)) / (ditherSize.xy * scale));
 	vec2 uvPreview = uv;
 	vec3 orig = texture2D(uSampler, uvPreview).rgb;
-	// vec3 col = (orig - 0.5 + (brightness - 1.0)) * contrast + 0.5;
-	// vec3 limit = texture2D(ditherGridMap, uvDither).rgb;
-	// // posterization
-	// vec3 raw = col;
-	// vec3 posterized = raw - mod(raw, 1.0/posterize);
-	// // dithering
-	// vec3 dither = step(limit, (raw-posterized)*posterize)/posterize;
-	// // output
-	// col = posterized + dither;
 
-	// col = mix(col, vec3(1.0), whiteout);
-	// col = mix(col, vec3(1.0) - col, invert);
+	vec3 col = (orig - 0.5 + (brightness - 1.0)) * contrast + 0.5;
+	vec3 limit = texture2D(ditherGridMap, uvDither).rgb;
 
-	// vec3 rgb = mix(bg, fg, col) / 255.0;
+	// posterization
+	vec3 raw = col;
+	vec3 posterized = raw - mod(raw, 1.0/posterize);
+	// dithering
+	vec3 dither = step(limit, (raw-posterized)*posterize)/posterize;
+	// output
+	col = posterized + dither;
 
-	// gl_FragColor = vec4(rgb, 1.0);
-	gl_FragColor = vec4(orig, 1.0);
+	col = mix(col, vec3(1.0), whiteout);
+	col = mix(col, vec3(1.0) - col, invert);
+
+	vec3 rgb = mix(bg, fg, col) / 255.0;
+
+	gl_FragColor = vec4(rgb, 1.0);
+	// gl_FragColor = vec4(orig, 1.0);
 }
