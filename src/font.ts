cb98fbd56f11ed5a6043f064166bc51ff1c4552e
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-06-24 22:35:49 -0400

    bigger fonts

--- src/font.ts
+++ src/font.ts
@@ -2,31 +2,31 @@ import type { ITextStyle } from 'pixi.js';
 
 export const fontDialogue: Partial<ITextStyle> = {
 	fontFamily: 'font',
-	fontSize: 8,
+	fontSize: 24,
 	padding: 16,
 	fill: 0xffffff,
 	align: 'left',
-	lineHeight: 8 * 1.25,
+	lineHeight: 24 * 1.25,
 	letterSpacing: 0,
 };
 export const fontPrompt: Partial<ITextStyle> = {
 	fontFamily: 'font',
-	fontSize: 8,
+	fontSize: 24,
 	padding: 16,
 	fill: 0xffffff,
 	align: 'center',
-	lineHeight: 8 * 1.25,
+	lineHeight: 24 * 1.25,
 	letterSpacing: 0,
 	stroke: 0,
 	strokeThickness: 2,
 };
 export const fontIngame: Partial<ITextStyle> = {
 	fontFamily: 'font',
-	fontSize: 8,
+	fontSize: 24,
 	padding: 16,
 	fill: 0xffffff,
 	align: 'center',
-	lineHeight: 8 * 1.25,
+	lineHeight: 24 * 1.25,
 	letterSpacing: 0,
 	stroke: 0,
 	strokeThickness: 2,
